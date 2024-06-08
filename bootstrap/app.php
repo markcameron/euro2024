a6dc3f0e54a8bdba12df7ef26b34535541527296
Mark Cameron <mark.cameron@devfactory.ch>
2024-06-08 22:14:28 +0200

    Add trust proxies for specific IP range

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: [
+            '172.18.0.0/16',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
