591a6fae1450fa5a93dc3904d43525e28613e6b9
Mark Cameron <mark.cameron@devfactory.ch>
2024-06-08 21:16:06 +0200

    Add RoadRunner configuration and Octane server setup

--- cmd.sh
+++ cmd.sh
@@ -2,5 +2,9 @@
 
 php artisan migrate --force -n
 
+./vendor/bin/rr get-binary
+
+chmod +x ./rr
+
 # rr serve -c ./.rr.yaml
 php -d variables_order=EGPCS artisan octane:start --server=roadrunner --host=0.0.0.0 --rpc-port=6001 --port=8080
