de4b25b273c13b3d385af0935240cfe932260628
Mark Cameron <mark.cameron@devfactory.ch>
2024-06-08 18:11:49 +0200

    Update rr config

--- cmd.sh
+++ cmd.sh
@@ -2,4 +2,5 @@
 
 php artisan migrate --force -n
 
-rr serve -c ./.rr.yaml
+# rr serve -c ./.rr.yaml
+/usr/bin/php -d variables_order=EGPCS /var/www/html/artisan octane:start --server=roadrunner --host=0.0.0.0 --rpc-port=6001 --port=8080
