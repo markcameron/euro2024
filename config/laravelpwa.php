e110885876d7f55bc8c8183ac88d0925e148990b
Mark Cameron <mark.cameron@devfactory.ch>
2024-06-09 17:01:40 +0200

    Update icon purposes to 'maskable any' and add Euro 2024 Predictor

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -14,31 +14,31 @@ return [
         'icons' => [
             '72x72' => [
                 'path' => '/images/icons/icon-72x72.png',
-                'purpose' => 'any'
+                'purpose' => 'maskable any'
             ],
             '96x96' => [
                 'path' => '/images/icons/icon-96x96.png',
-                'purpose' => 'any'
+                'purpose' => 'maskable any'
             ],
             '128x128' => [
                 'path' => '/images/icons/icon-128x128.png',
-                'purpose' => 'any'
+                'purpose' => 'maskable any'
             ],
             '144x144' => [
                 'path' => '/images/icons/icon-144x144.png',
-                'purpose' => 'any'
+                'purpose' => 'maskable any'
             ],
             '152x152' => [
                 'path' => '/images/icons/icon-152x152.png',
-                'purpose' => 'any'
+                'purpose' => 'maskable any'
             ],
             '192x192' => [
                 'path' => '/images/icons/icon-192x192.png',
-                'purpose' => 'any'
+                'purpose' => 'maskable any'
             ],
             '512x512' => [
                 'path' => '/images/icons/icon-512x512.png',
-                'purpose' => 'any'
+                'purpose' => 'maskable any'
             ],
         ],
         'splash' => [
@@ -55,19 +55,14 @@ return [
         ],
         'shortcuts' => [
             [
-                'name' => 'Shortcut Link 1',
-                'description' => 'Shortcut Link 1 Description',
-                'url' => '/shortcutlink1',
+                'name' => 'Euro 2024 Predictor',
+                'description' => 'Access the Euro 2024 Predictor',
+                'url' => '/',
                 'icons' => [
                     "src" => "/images/icons/icon-72x72.png",
                     "purpose" => "any"
                 ]
-            ],
-            // [
-            //     'name' => 'Shortcut Link 2',
-            //     'description' => 'Shortcut Link 2 Description',
-            //     'url' => '/shortcutlink2'
-            // ]
+                ],
         ],
         'custom' => []
     ]
