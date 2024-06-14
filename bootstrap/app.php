8ffcf77690a25b0e6ebf4eafef9fcb602918f328
Mark Cameron <mark.cameron@devfactory.ch>
2024-06-14 11:18:51 +0200

    Enable UpdateLastSeen middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\UpdateLastSeen;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,9 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: [
-            '172.18.0.0/16',
-        ]);
+        $middleware
+            ->trustProxies(at: [
+                '172.18.0.0/16',
+            ])
+            ->append(UpdateLastSeen::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
